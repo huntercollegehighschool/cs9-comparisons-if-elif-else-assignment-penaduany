e6d871e071f960658764e7d83c09a41731e040cc
Duany Pena <94643825+penaduany@users.noreply.github.com>
2021-12-03 23:12:01 +0000

    i defined "number 3" and made it so that if number 3 is less than the smallest number of the previous two, then it is the new smallest number

--- part1.py
+++ part1.py
@@ -24,4 +24,9 @@ number = int(input("Enter another number: "))
 if number < smallest:
   smallest = number
   
-print("The smallest number is ", smallest)
+number3 = int(input("Enter another number: "))
+
+if number3 < smallest:
+  smallest = number3
+
+print("The smallest number is", smallest)
